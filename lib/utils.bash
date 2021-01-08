fb2f064ea68155fe1f8be79e9491072c726e6417
Stephen Nowell <stephen@stardog.com>
2021-01-08 14:48:05 -0500

    One more try at fixing install

--- lib/utils.bash
+++ lib/utils.bash
@@ -49,7 +49,7 @@ install_version() {
     # TODO: Asert stardog executable exists.
     local tool_cmd
     tool_cmd="$(echo "stardog version" | cut -d' ' -f1)"
-    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+    test -x "$install_path/stardog-$version/bin/$tool_cmd" || fail "Expected $install_path/stardog-$version/bin/$tool_cmd to be executable."
 
     echo "stardog $version installation was successful!"
   ) || (
